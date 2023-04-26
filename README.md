76355e5bed9cb1dffa7c306b89e32176f2829c2a
arjunyel <arjunyel@gmail.com>
2023-04-25 22:52:36 -0500

    Replicate bug

--- README.md
+++ README.md
@@ -1,22 +1,9 @@
-# Welcome to Remix!
+1. `npm install`
 
-- [Remix Docs](https://remix.run/docs)
+1. Rename `.dev.vars.example` to `.dev.vars` and fill out with Turso variables
 
-## Development
+1. Modify the query in `app/routes/_index.tsx`
 
-You will be utilizing Wrangler for local development to emulate the Cloudflare runtime. This is already wired up in your package.json as the `dev` script:
+1. `npm run dev`
 
-```sh
-# start the remix dev server and wrangler
-npm run dev
-```
-
-Open up [http://127.0.0.1:8788](http://127.0.0.1:8788) and you should be ready to go!
-
-## Deployment
-
-Cloudflare Pages are currently only deployable through their Git provider integrations.
-
-If you don't already have an account, then [create a Cloudflare account here](https://dash.cloudflare.com/sign-up/pages) and after verifying your email address with Cloudflare, go to your dashboard and follow the [Cloudflare Pages deployment guide](https://developers.cloudflare.com/pages/framework-guides/deploy-anything).
-
-Configure the "Build command" should be set to `npm run build`, and the "Build output directory" should be set to `public`.
+1. Try to navigate to [http://127.0.0.1:8788/](http://127.0.0.1:8788/), it hangs forever
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,38 +1,49 @@
-import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import type { LoaderFunction, V2_MetaFunction } from "@remix-run/cloudflare";
+import { createClient } from "@libsql/client/web";
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "New Remix App" }];
+	return [{ title: "New Remix App" }];
+};
+
+export const loader: LoaderFunction = async ({ context }) => {
+	console.log("Starting query");
+	const test = await createClient({
+		url: context.TURSO_DATABASE_URL as string,
+		authToken: context.TURSO_AUTH_TOKEN as string,
+	}).execute(`select * from users`);
+	console.log("results", test);
+	return test;
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+			<h1>Welcome to Remix</h1>
+			<ul>
+				<li>
+					<a
+						target="_blank"
+						href="https://remix.run/tutorials/blog"
+						rel="noreferrer"
+					>
+						15m Quickstart Blog Tutorial
+					</a>
+				</li>
+				<li>
+					<a
+						target="_blank"
+						href="https://remix.run/tutorials/jokes"
+						rel="noreferrer"
+					>
+						Deep Dive Jokes App Tutorial
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+						Remix Docs
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
 }
